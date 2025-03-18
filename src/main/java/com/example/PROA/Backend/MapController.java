a13da04dfcb2cd6584c575fbbe00967280aa6843
Saurabh Uchil <uchilsaurabh@gmail.com>
2025-03-19 01:17:06 +1100

    Enable CORS

--- src/main/java/com/example/PROA/Backend/MapController.java
+++ src/main/java/com/example/PROA/Backend/MapController.java
@@ -3,6 +3,7 @@ package com.example.PROA.Backend;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class MapController {
 	@Autowired
 	VariablesRepo variablesRepo;
 	
-	
+	@CrossOrigin(origins = "http://localhost:3000") // Enable CORS for this endpoint
 	@GetMapping("/weatherstations")
 	public List<WeatherStations> showWeatherStations() {
 		
@@ -23,12 +24,14 @@ public class MapController {
 		return weatherStationsList;
 	}
 	
+	@CrossOrigin(origins = "http://localhost:3000") // Enable CORS for this endpoint
 	@GetMapping("/variables")
 	public List<Variables> showVariables() {
 		List<Variables> variablesList = variablesRepo.findAll();
 		return variablesList;
 	}
 	
+	@CrossOrigin(origins = "http://localhost:3000") // Enable CORS for this endpoint
 	@GetMapping("/data")
 	public Data showData() {
 		Data d1 = new Data(120.9, 21.88, "29/08/2023  2:05:00 AM" );
